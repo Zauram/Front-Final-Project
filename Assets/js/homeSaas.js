69b5069edf382433581238e40a7407cdeee8f3ae
Zauram <zauram@code.edu.az>
2022-09-02 19:16:51 +0400

    Commit-5

---
+++ Assets/js/homeSaas.js
@@ -0,0 +1,30 @@
+window.onscroll = function () {
+    var header = document.querySelector(".navbar-field");
+    var sticky = header.offsetTop;
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+};
+AOS.init();
+
+let counter = document.querySelector(".row .row");
+let counter_Up = new counterUp(counter);
+counter_Up.start();
+counter_Up.update();
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    items: 2,
+    responsive: {
+        600: {
+            items: 1,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
